0a41575d09606aef8df6302842a86218810b4259
hideki watabe <hideki.w.1971@gmail.com>
2022-09-24 12:47:51 +0900

    2022/9/24 一応動くようになった。

--- js/script.js
+++ js/script.js
@@ -1,50 +1,26 @@
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
-  var topBtn = $('.pagetop');
-  topBtn.hide();
-
-  // ボタンの表示設定
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 70) {
-      // 指定px以上のスクロールでボタンを表示
-      topBtn.fadeIn();
-    } else {
-      // 画面が指定pxより上ならボタンを非表示
-      topBtn.fadeOut();
-    }
+  const mySwiper = new Swiper(".card01 .swiper", {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    grabCursor: true,
+    pagination: {
+      el: ".card01 .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".card01 .swiper-button-next",
+      prevEl: ".card01 .swiper-button-prev",
+    },
+    breakpoints: {
+      600: {
+        slidesPerView: 2,
+      },
+      1025: {
+        slidesPerView: 4,
+        spaceBetween: 32,
+      },
+    },
   });
-
-  // ボタンをクリックしたらスクロールして上に戻る
-  topBtn.click(function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 300, 'swing');
-    return false;
-  });
-
-  //ドロワーメニュー
-  $("#MenuButton").click(function () {
-    // $(".l-drawer-menu").toggleClass("is-show");
-    // $(".p-drawer-menu").toggleClass("is-show");
-    $(".js-drawer-open").toggleClass("open");
-    $(".drawer-menu").toggleClass("open");
-    $("html").toggleClass("is-fixed");
-
-  });
-
-
-
-  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
-
-  $(document).on('click', 'a[href*="#"]', function () {
-    let time = 400;
-    let header = $('header').innerHeight();
-    let target = $(this.hash);
-    if (!target.length) return;
-    let targetY = target.offset().top - header;
-    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
-    return false;
-  });
-
 });
